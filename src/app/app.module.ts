9e5a9649fb86dc6d1f8e277f650cafde191e32e9
Matt Mcnair <mmcnair@lat-mmcnair1.cb.careerbuilder.com>
2017-03-06 14:38:55 -0500

    adding the whole project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
+// our feature model
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module';
+
+// our app component
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,8 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    TeslaBatteryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
